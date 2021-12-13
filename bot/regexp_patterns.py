3a7175446cc6caf9206d86f93f4de1b4c0f13bc6
ipetrash <ilya.petrash@inbox.ru>
2021-12-14 01:22:13 +0500

    Обновлено. Добавлен шаблон файла "regexp_patterns.py"

---
+++ bot/regexp_patterns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+
+# TODO:
+# NOTE: Examples:
+# PATTERN_PLAYLIST_ID = re.compile('^playlist_id=(.+)$')
+# PATTERN_GENERATE_RANDOM_PASSWORD = re.compile('^generate_random_password$')
+# PATTERN_CANCEL = re.compile('^cancel$')
+# PATTERN_GET_BOT_PASSWORD = re.compile('^Get bot password$')
+# PATTERN_SET_BOT_PASSWORD = re.compile('^Set bot password$')
