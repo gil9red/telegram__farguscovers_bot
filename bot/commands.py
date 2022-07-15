2668497c7fb856bc8843a0ba3699ddff7c1f4aaf
gil9red <ilya.petrash@inbox.ru>
2022-07-15 20:43:38 +0500

    Исправление текста в /start

--- bot/commands.py
+++ bot/commands.py
@@ -97,10 +97,10 @@ def on_start(update: Update, context: CallbackContext):
     text = (
         'Бот для отображения обложек с стены группы ВК https://vk.com/farguscovers\n\n'
         f'Всего {Cover.count()} обложек за период '
-        f'{Cover.get_first().date_time.year}-{Cover.get_last().date_time.year}\n\n',
+        f'{Cover.get_first().date_time.year}-{Cover.get_last().date_time.year}\n\n'
         'Чтобы открыть обложку по номеру, просто введите номер\n'
         'В боте ссылки используются для перехода к сущностям. '
-        'После клика на ссылку ниже появится кнопка запуска на которую нужно кликнуть.',
+        'После клика на ссылку ниже появится кнопка запуска на которую нужно кликнуть.'
     )
     reply_message(
         text,
