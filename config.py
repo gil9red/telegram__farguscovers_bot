7f5b20b9e6db90b96acadcd639918404f6b1955d
gil9red <ilya.petrash@inbox.ru>
2023-04-18 16:21:59 +0500

    Рефакторинг. Использование стиля кода black

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
@@ -13,47 +13,51 @@ from pathlib import Path
 # Текущая папка, где находится скрипт
 DIR = Path(__file__).resolve().parent
 
-DIR_LOGS = DIR / 'logs'
+DIR_LOGS = DIR / "logs"
 DIR_LOGS.mkdir(parents=True, exist_ok=True)
 
-DIR_DATA_VK = DIR / 'data_vk'
+DIR_DATA_VK = DIR / "data_vk"
 
-FILE_NAME_DUMP = DIR_DATA_VK / 'dump.json'
+FILE_NAME_DUMP = DIR_DATA_VK / "dump.json"
 if not FILE_NAME_DUMP.exists():
-    raise Exception(f'Отсутствует экспортированный файл обложек: {FILE_NAME_DUMP}')
+    raise Exception(f"Отсутствует экспортированный файл обложек: {FILE_NAME_DUMP}")
 
-DIR_IMAGES = DIR_DATA_VK / 'images'
-if not DIR_IMAGES.exists() or not any(DIR_IMAGES.glob('*.jpg')):
-    raise Exception(f'Отсутствует или пустая папка с картинками: {DIR_IMAGES}')
+DIR_IMAGES = DIR_DATA_VK / "images"
+if not DIR_IMAGES.exists() or not any(DIR_IMAGES.glob("*.jpg")):
+    raise Exception(f"Отсутствует или пустая папка с картинками: {DIR_IMAGES}")
 
-TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+TOKEN_FILE_NAME = DIR / "TOKEN.txt"
 
 try:
-    TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
+    TOKEN = os.environ.get("TOKEN") or TOKEN_FILE_NAME.read_text("utf-8").strip()
     if not TOKEN:
-        raise Exception('TOKEN пустой!')
+        raise Exception("TOKEN пустой!")
 
 except:
-    print(f'Нужно в {TOKEN_FILE_NAME.name} или в переменную окружения TOKEN добавить токен бота')
+    print(
+        f"Нужно в {TOKEN_FILE_NAME.name} или в переменную окружения TOKEN добавить токен бота"
+    )
     TOKEN_FILE_NAME.touch()
     sys.exit()
 
-SCREENSHOT_GIF_START_DEEP_LINKING = DIR / 'etc' / 'screenshots' / 'start_deep_linking.gif'
+SCREENSHOT_GIF_START_DEEP_LINKING = (
+    DIR / "etc" / "screenshots" / "start_deep_linking.gif"
+)
 if not SCREENSHOT_GIF_START_DEEP_LINKING.exists():
-    raise Exception(f'Отсутствует файл: {SCREENSHOT_GIF_START_DEEP_LINKING}')
+    raise Exception(f"Отсутствует файл: {SCREENSHOT_GIF_START_DEEP_LINKING}")
 
-ERROR_TEXT = 'Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже...'
-PLEASE_WAIT = 'Пожалуйста, подождите...'
+ERROR_TEXT = "Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже..."
+PLEASE_WAIT = "Пожалуйста, подождите..."
 
 # Создание папки для базы данных
-DB_DIR_NAME = DIR / 'database'
+DB_DIR_NAME = DIR / "database"
 DB_DIR_NAME.mkdir(parents=True, exist_ok=True)
 
 # Путь к файлу базы данных
-DB_FILE_NAME = str(DB_DIR_NAME / 'database.sqlite')
+DB_FILE_NAME = str(DB_DIR_NAME / "database.sqlite")
 
 USER_NAME_ADMINS = [
-    '@ilya_petrash',
+    "@ilya_petrash",
 ]
 
 MAX_MESSAGE_LENGTH = 4096
@@ -61,4 +65,4 @@ ITEMS_PER_PAGE = 10
 
 DEFAULT_AUTHOR_ID = 0
 DEFAULT_AUTHOR_NAME = 'Обложки "Фаргус"'
-DEFAULT_AUTHOR_URL = 'https://vk.com/farguscovers'
+DEFAULT_AUTHOR_URL = "https://vk.com/farguscovers"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
@@ -18,11 +18,11 @@ from config import TOKEN
 
 
 def main():
-    log.debug('Start')
+    log.debug("Start")
 
     cpu_count = os.cpu_count()
     workers = cpu_count
-    log.debug(f'System: CPU_COUNT={cpu_count}, WORKERS={workers}')
+    log.debug(f"System: CPU_COUNT={cpu_count}, WORKERS={workers}")
 
     updater = Updater(
         workers=workers,
@@ -33,7 +33,7 @@ def main():
         ),
     )
     bot = updater.bot
-    log.debug(f'Bot name {bot.first_name!r} ({bot.name})')
+    log.debug(f"Bot name {bot.first_name!r} ({bot.name})")
 
     dp = updater.dispatcher
     commands.setup(dp)
@@ -41,16 +41,16 @@ def main():
     updater.start_polling()
     updater.idle()
 
-    log.debug('Finish')
+    log.debug("Finish")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
         try:
             main()
         except:
-            log.exception('')
+            log.exception("")
 
             timeout = 15
-            log.info(f'Restarting the bot after {timeout} seconds')
+            log.info(f"Restarting the bot after {timeout} seconds")
             time.sleep(timeout)
